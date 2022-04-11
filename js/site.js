a031b24c42a4e69af438575e0c632a61c5855ede
Foster0x <sfoster2010.sf@gmail.com>
2022-04-11 15:42:09 -0400

    working app with first js

--- js/site.js
+++ js/site.js
@@ -1,20 +1,43 @@
 // Sep One - controller/accepts requests
-function getMessage() {
-    let msg = document.getElementById("txtMessage").value;
-    displayMessage(msg);
+function getValues() {
+    // Get values from user
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+    // Check for numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        displayValues(fizzValue, buzzValue);
+    } else {
+        alert("You must enter integers!");
+    }
 }
-
 // Final Step - view
-function displayMessage(message) {
-
-    // First get the ol element from the page
+function displayValues(fizz, buzz) {
     element = document.getElementById("results");
-    // Next creat a new li element
-    let item = document.createElement("li");
-    // Add classes to the li element
-    item.classList.add("list-group-item");
-    // Set the message for the li element
-    item.innerHTML = message;
-    // Add the new item to the list 
-    element.appendChild(item);
+    // For loop to display 0-100
+    for (let i = 1; i < 101; i++) {
+        let item = document.createElement("div");
+        item.classList.add("col");
+        // Test if divisible by fizz and buzz-If true replace with FIZZBUZZ
+        if (i % 3 == 0 && i % 5 == 0) {
+            item.classList.add("fizzbuzz")
+            item.innerHTML = 'FizzBuzz';
+        }
+        // Test if divisible by fizz only-If true replace with FIZZ
+        else if (i % 3 == 0) {
+            item.classList.add("fizz");
+            item.innerHTML = 'Fizz';
+        }
+        // Test if divisible by buzz only-If true replace with BUZZ
+        else if (i % 5 == 0) {
+            item.classList.add("buzz");
+            item.innerHTML = 'Buzz';
+        }
+        // Finally just print number
+        else {
+            item.innerHTML = i;
+        }
+        element.appendChild(item);
+    }
 }
